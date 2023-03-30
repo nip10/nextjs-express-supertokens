1d1c8f0174aef1437c6e1e86ad51775a71fb250d
Diogo Cardoso <diogocardoso92@outlook.com>
2023-03-30 17:14:24 +0100

    add useSessionContext example

--- client/pages/private.tsx
+++ client/pages/private.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
-import { SessionAuth } from "supertokens-auth-react/recipe/session";
+import {
+  SessionAuth,
+  useSessionContext,
+} from "supertokens-auth-react/recipe/session";
 
 const PrivatePage: NextPage = () => {
+  const session = useSessionContext();
+  if (session.loading === true) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <SessionAuth>
       <div>This is a private page</div>
+      <p>User id: {session.userId} </p>
     </SessionAuth>
   );
 };
